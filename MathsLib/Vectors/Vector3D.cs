599aea55b88a66d841357efa1b30e8f73158850e
Lady_Synestia <chloeeejohnson@outlook.com>
2025-05-18 14:24:04 +0100

    Vector Doc comments

--- MathsLib/Vectors/Vector3D.cs
+++ MathsLib/Vectors/Vector3D.cs
@@ -2,35 +2,46 @@
 
 namespace MathsLib
 {
-    public sealed partial record Vector3D(float x, float y, float z)
+    public sealed partial record Vector3D(double x, double y, double z)
     {
-        public float x { get; } = x;
-        public float y { get; } = y;
-        public float z { get; } = z;
+        public double x { get; } = x;
+        public double y { get; } = y;
+        public double z { get; } = z;
+        
 
-        // copy constructor
-        //public Vector3D(Vector3D a) : this (a.x, a.y, a.z) { }
-
-        // implicit conversion from 4D Vector
+        /// <summary>
+        /// implicit conversion from 4D Vector
+        /// </summary>
+        /// <param name="v">4D Vector</param>
+        /// <returns></returns>
         public static implicit operator Vector3D(Vector4D v) => new(v.x, v.y, v.z);
 
         // x: pitch, y: yaw, z: roll
         public static Vector3D FromAngles(Vector3D angles) => new(
-            MathF.Cos(angles.y) * MathF.Cos(angles.x),
-            MathF.Sin(angles.x),
-            MathF.Cos(angles.x) * MathF.Sin(angles.y));
+            Math.Cos(angles.y) * Math.Cos(angles.x),
+            Math.Sin(angles.x),
+            Math.Cos(angles.x) * Math.Sin(angles.y));
 
-        public static Vector3D FromAngles2D(float roll) => new(
-            MathF.Cos(roll),
-            MathF.Sin(roll),
+        public static Vector3D FromAngles2D(double roll) => new(
+            Math.Cos(roll),
+            Math.Sin(roll),
             0);
 
-
+        /// <summary>
+        /// Custom ToString Method, truncating values to a specified number of digits
+        /// </summary>
+        /// <param name="digits">Number of digits to include</param>
+        /// <returns></returns>
         public string ToString(int digits) => $"{x.ToString(digits)}, {y.ToString(digits)}, {z.ToString(digits)}";
+        
+        /// <summary>
+        /// Default ToString method override
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() => $"{x}, {y}, {z}";
         
         // return values as tuple
-        public (float x, float y, float z) Tuple => (x, y, z);
+        public (double x, double y, double z) Tuple => (x, y, z);
     }
 
 
@@ -38,74 +49,170 @@ namespace MathsLib
     {
         // vector operations
 
-        // using pythagoras
-        public float Magnitude => MathF.Sqrt(x * x + y * y + z * z);
-        public float SqrMagnitude => x * x + y * y + z * z;
+        /// <summary>
+        /// Vector Magnitude
+        /// </summary>
+        public double Magnitude => Math.Sqrt(x * x + y * y + z * z);
+        
+        /// <summary>
+        /// Vector Magnitude Squared
+        /// </summary>
+        public double SqrMagnitude => x * x + y * y + z * z;
+        
+        /// <summary>
+        /// Normalised form of the vector
+        /// </summary>
         public Vector3D Normalised => Normalise(this);
         
+        /// <summary>
+        /// Calculates normalised (unit) form of a vector
+        /// </summary>
+        /// <param name="a">Vector to normalise</param>
+        /// <returns>normalised vector</returns>
         public static Vector3D Normalise(Vector3D a) => a / a.Magnitude;
 
-        // comparing two Vectors
-        public static bool Equals(Vector3D a, Vector3D b) => MathF.Abs(a.x - b.x) < Maths.Tolerance &&
-                                                             MathF.Abs(a.y - b.y) < Maths.Tolerance &&
-                                                             MathF.Abs(a.z - b.z) < Maths.Tolerance;
-        //public static bool operator ==(Vector3D a, Vector3D b) => Equals(a, b);
-        //public static bool operator !=(Vector3D a, Vector3D b) => !Equals(a, b);
+        /// <summary>
+        /// Custom vector equality comparison. accounts for doubleing point error margins
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns></returns>
+        public static bool Equals(Vector3D a, Vector3D b) => Math.Abs(a.x - b.x) < Maths.Tolerance &&
+                                                             Math.Abs(a.y - b.y) < Maths.Tolerance &&
+                                                             Math.Abs(a.z - b.z) < Maths.Tolerance;
 
-        // Dot product of two vectors
-        public static float Dot(Vector3D a, Vector3D b) => (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+        /// <summary>
+        /// Dot product of two vectors
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns></returns>
+        public static double Dot(Vector3D a, Vector3D b) => (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
         
-        public float Dot(Vector3D b) => Dot(this, b); // concise syntax
+        /// <summary>
+        /// Dot product of two vectors. LHS is vector performed upon.
+        /// </summary>
+        /// <param name="b">RHS</param>
+        /// <returns></returns>
+        public double Dot(Vector3D b) => Dot(this, b);
 
-        // Distance between two vector positions
-        public static float Distance(Vector3D a, Vector3D b) => MathF.Sqrt(DistanceSquared(a, b));
+        /// <summary>
+        /// Distance between two vector positions
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns></returns>
+        public static double Distance(Vector3D a, Vector3D b) => Math.Sqrt(DistanceSquared(a, b));
         
-        public static float DistanceSquared(Vector3D a, Vector3D b)
+        /// <summary>
+        /// Distance squared between two vector positions
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns></returns>
+        public static double DistanceSquared(Vector3D a, Vector3D b)
         {
-            float dx = a.x - b.x;
-            float dy = a.y - b.y;
-            float dz = a.z - b.z;
+            double dx = a.x - b.x;
+            double dy = a.y - b.y;
+            double dz = a.z - b.z;
 
             return dx * dx + dy * dy + dz * dz;
         }
 
-        // Angle between two vectors
-        public static float Angle(Vector3D a, Vector3D b) => MathF.Acos(Dot(a, b) / (a.Magnitude * b.Magnitude));
+        /// <summary>
+        /// Angle between two vectors
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns>Angle in radians</returns>
+        public static double Angle(Vector3D a, Vector3D b) => Math.Acos(Dot(a, b) / (a.Magnitude * b.Magnitude));
 
-        // Cross product of two vectors
+        /// <summary>
+        /// Cross product of two vectors
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns></returns>
         public static Vector3D Cross(Vector3D a, Vector3D b) => new(
             a.y * b.z - a.z * b.y,
             a.z * b.x - a.x * b.z,
             a.x * b.y - a.y * b.x);
 
-        // clamps the magnitude of a Vector, returns new vector
-        public static Vector3D ClampMagnitude(Vector3D a, float maxlength) =>
-            a.Magnitude > maxlength ? Normalise(a) * maxlength : a;
+        /// <summary>
+        /// clamps the magnitude of a Vector to a specified value
+        /// </summary>
+        /// <param name="a">Vector to clamp</param>
+        /// <param name="maxlength">value to clamp to</param>
+        /// <returns>Clamped vector</returns>
+        public static Vector3D ClampMagnitude(Vector3D a, double maxlength) => a.Magnitude > maxlength ? Normalise(a) * maxlength : a;
 
-        // calculates the midpoint of two vectors - same as Lerp(a, b, 0.5)
+        /// <summary>
+        /// calculates the midpoint of two vectors - same as Lerp(a, b, 0.5)
+        /// </summary>
+        /// <param name="a">Vector From</param>
+        /// <param name="b">Vector To</param>
+        /// <returns></returns>
         public static Vector3D Midpoint(Vector3D a, Vector3D b) => a + (b - a) * 0.5f;
 
-        // calculates point on the line between a and b
-        public static Vector3D Lerp(Vector3D a, Vector3D b, float t) => a + (b - a) * t;
+        /// <summary>
+        /// Calculates a point a specified distance along the line between a and b
+        /// </summary>
+        /// <param name="a">Vector from</param>
+        /// <param name="b">Vector to</param>
+        /// <param name="t">fractional amount of total distance</param>
+        /// <returns></returns>
+        public static Vector3D Lerp(Vector3D a, Vector3D b, double t) => a + (b - a) * t;
 
-        // calculates the vector from a to b
+        /// <summary>
+        /// Calculates the vector from a to b
+        /// </summary>
+        /// <param name="a">Vector From</param>
+        /// <param name="b">Vector To</param>
+        /// <returns></returns>
         public static Vector3D Between(Vector3D a, Vector3D b) => b - a;
-        public Vector3D From(Vector3D other) => Between(other, this); // concise syntax for second operand
-        public Vector3D To(Vector3D other) => Between(this, other); // concise syntax for first operand
+        
+        /// <summary>
+        /// Calculates vector From another vector To this vector
+        /// </summary>
+        /// <param name="other">Vector from</param>
+        /// <returns></returns>
+        public Vector3D From(Vector3D other) => Between(other, this);
+        
+        /// <summary>
+        /// Calculates vector To another vector From this vector
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public Vector3D To(Vector3D other) => Between(this, other);
 
-        // returns a vector with the largest values of each of the vectors
+        /// <summary>
+        /// Returns a vector with the largest values for each component of each of the vectors
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>Maximised vector</returns>
         public static Vector3D Max(Vector3D a, Vector3D b) => new(
-            MathF.Max(a.x, b.x),
-            MathF.Max(a.y, b.y),
-            MathF.Max(a.z, b.z));
+            Math.Max(a.x, b.x),
+            Math.Max(a.y, b.y),
+            Math.Max(a.z, b.z));
 
-        // returns a vector with the smallest values of each of the vectors
+        /// <summary>
+        /// Returns a vector with the smalled values for each component of each of the vectors
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>Minimised vector</returns>
         public static Vector3D Min(Vector3D a, Vector3D b) => new(
-            MathF.Min(a.x, b.x),
-            MathF.Min(a.y, b.y),
-            MathF.Min(a.z, b.z));
+            Math.Min(a.x, b.x),
+            Math.Min(a.y, b.y),
+            Math.Min(a.z, b.z));
 
-        // projects a vector onto another vector
+        /// <summary>
+        /// Projects a vector onto another vector
+        /// </summary>
+        /// <param name="a">Vector to project</param>
+        /// <param name="b">Vector projected to</param>
+        /// <returns>Projected vector</returns>
         public static Vector3D Project(Vector3D a, Vector3D b) => b * Dot(a, b);
     }
 
@@ -119,19 +226,20 @@ namespace MathsLib
             a.y * b.y,
             a.z * b.z);
 
-        public static Vector3D operator *(Vector3D v, float s) => new(
+        public static Vector3D operator *(Vector3D v, double s) => new(
             v.x * s,
             v.y * s,
             v.z * s);
 
-        public static Vector3D operator *(float s, Vector3D v) => v * s;
+        public static Vector3D operator *(double s, Vector3D v) => v * s;
 
+        // Scalar Division of vectors
         public static Vector3D operator /(Vector3D a, Vector3D b) => new(
             a.x / b.x,
             a.y / b.y,
             a.z / b.z);
 
-        public static Vector3D operator /(Vector3D a, float s) => new(
+        public static Vector3D operator /(Vector3D a, double s) => new(
             a.x / s,
             a.y / s,
             a.z / s);
--- MathsLib/Vectors/Vector4D.cs
+++ MathsLib/Vectors/Vector4D.cs
@@ -4,44 +4,68 @@ namespace MathsLib
 {
     public sealed partial record Vector4D
     {
-        public float x { get; }
-        public float y { get; }
-        public float z { get; }
-        public float w { get; }
-
-        /*public Vector4D(Vector4D a) : this (
-            a.x,
-            a.y,
-            a.z,
-            a.w) {}*/
-
-        public Vector4D(Vector3D a, float w = 1) : this(
-            a.x,
-            a.y,
-            a.z,
-            w) { }
-
-        public Vector4D(float X, float Y, float Z, float W)
+        public double x { get; }
+        public double y { get; }
+        public double z { get; }
+        public double w { get; }
+        
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="X">x component</param>
+        /// <param name="Y">y component</param>
+        /// <param name="Z">z component</param>
+        /// <param name="W">w component</param>
+        public Vector4D(double X, double Y, double Z, double W)
         {
             x = X;
             y = Y;
             z = Z;
             w = W;
         }
+        
+        /// <summary>
+        /// Constructor from 3D Vector with optional w component.
+        /// Used when w != 0.
+        /// </summary>
+        /// <param name="a">3D Vector</param>
+        /// <param name="w">w component, defaults to 1</param>
+        public Vector4D(Vector3D a, double w = 1) : this(
+            a.x,
+            a.y,
+            a.z,
+            w) { }
 
-        public (float x, float y, float z, float w) Tuple => (x, y, z, w);
+        /// <summary>
+        /// Returns components in the form of a tuple
+        /// </summary>
+        public (double x, double y, double z, double w) Tuple => (x, y, z, w);
 
-        // implicit conversion from 3D vector
+        /// <summary>
+        /// Implicit conversion from 3D vector. w = 0.
+        /// </summary>
+        /// <param name="v">3D Vector</param>
+        /// <returns></returns>
         public static implicit operator Vector4D(Vector3D v) => new(
             v.x,
             v.y,
             v.z,
             0);
 
+        /// <summary>
+        /// Custom ToString Method, truncates to a specified number of digits
+        /// </summary>
+        /// <param name="digits">Number of digits to include</param>
+        /// <returns></returns>
         public string ToString(int digits) => $"{x.ToString(digits)}, {y.ToString(digits)}, {z.ToString(digits)}, {w.ToString(digits)}";
+        
+        /// <summary>
+        /// Override of default ToString method
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() => $"{x}, {y}, {z}, {w}";
 
-        public void Deconstruct(out float x, out float y, out float z, out float w)
+        public void Deconstruct(out double x, out double y, out double z, out double w)
         {
             x = this.x;
             y = this.y;
@@ -54,19 +78,49 @@ namespace MathsLib
 
     public sealed partial record Vector4D
     {
-        public float Magnitude => MathF.Sqrt(x * x + y * y + z * z + w * w);
+        /// <summary>
+        /// Vector Magnitude
+        /// </summary>
+        public double Magnitude => Math.Sqrt(x * x + y * y + z * z + w * w);
 
+        /// <summary>
+        /// Normalised form of the vector
+        /// </summary>
         public Vector4D Normalised => Normalise(this);
+       
+        /// <summary>
+        /// Normalised (Unit) Vector
+        /// </summary>
+        /// <param name="a">Vector to normalise</param>
+        /// <returns></returns>
         public static Vector4D Normalise(Vector4D a) => a / a.Magnitude;
 
+        /// <summary>
+        /// Vector Equality Check
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns></returns>
         public static bool Equals(Vector4D a, Vector4D b) =>
-            MathF.Abs(a.x - b.x) < Maths.Tolerance && MathF.Abs(a.y - b.y) < Maths.Tolerance &&
-            MathF.Abs(a.z - b.z) < Maths.Tolerance &&
-            MathF.Abs(a.w - b.w) < Maths.Tolerance;
+            Math.Abs(a.x - b.x) < Maths.Tolerance && Math.Abs(a.y - b.y) < Maths.Tolerance &&
+            Math.Abs(a.z - b.z) < Maths.Tolerance &&
+            Math.Abs(a.w - b.w) < Maths.Tolerance;
         
-        public static float Dot(Vector4D a, Vector4D b) => a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+        /// <summary>
+        /// Dot product of two Vectors
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns></returns>
+        public static double Dot(Vector4D a, Vector4D b) => a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
 
-        public static float Angle(Vector4D a, Vector4D b) => MathF.Acos(Dot(a, b) / (a.Magnitude * b.Magnitude));
+        /// <summary>
+        /// Angle between two Vectors
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns>Angle in radians</returns>
+        public static double Angle(Vector4D a, Vector4D b) => Math.Acos(Dot(a, b) / (a.Magnitude * b.Magnitude));
 
     }
 
@@ -79,13 +133,13 @@ namespace MathsLib
             a.z * b.z,
             a.w * b.w);
 
-        public static Vector4D operator *(Vector4D a, float s) => new(
+        public static Vector4D operator *(Vector4D a, double s) => new(
             a.x * s,
             a.y * s,
             a.z * s,
             a.w * s);
 
-        public static Vector4D operator *(float s, Vector4D a) => a * s;
+        public static Vector4D operator *(double s, Vector4D a) => a * s;
 
         public static Vector4D operator /(Vector4D a, Vector4D b) => new(
             a.x / b.x,
@@ -93,7 +147,7 @@ namespace MathsLib
             a.z / b.z,
             a.w / b.w);
 
-        public static Vector4D operator /(Vector4D a, float s) => new(
+        public static Vector4D operator /(Vector4D a, double s) => new(
             a.x / s,
             a.y / s,
             a.z / s,
